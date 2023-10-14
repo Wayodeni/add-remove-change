0f1229a96d5d7638a7aac854f5b18554ae51438f
lev sviridov <levgeneral@yandex.ru>
2023-10-14 12:47:35 +0300

    initial

---
+++ src/App.jsx
@@ -0,0 +1,133 @@
+import React, { useState } from "react";
+import { DndProvider } from "react-dnd";
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import Button from "@mui/material/Button";
+import AddIcon from "@mui/icons-material/Add";
+import {
+  Tree,
+  MultiBackend,
+  getDescendants,
+  getBackendOptions
+} from "@minoru/react-dnd-treeview";
+import { CustomNode } from "./CustomNode";
+import { CustomDragPreview } from "./CustomDragPreview";
+import { AddDialog } from "./AddDialog";
+import { theme } from "./theme";
+import styles from "./App.module.css";
+import SampleData from "./sample_data.json";
+
+const getLastId = (treeData) => {
+  const reversedArray = [...treeData].sort((a, b) => {
+    if (a.id < b.id) {
+      return 1;
+    } else if (a.id > b.id) {
+      return -1;
+    }
+
+    return 0;
+  });
+
+  if (reversedArray.length > 0) {
+    return reversedArray[0].id;
+  }
+
+  return 0;
+};
+
+function App() {
+  const [treeData, setTreeData] = useState(SampleData);
+  const handleDrop = (newTree) => setTreeData(newTree);
+  const [open, setOpen] = useState(false);
+
+  const handleDelete = (id) => {
+    const deleteIds = [
+      id,
+      ...getDescendants(treeData, id).map((node) => node.id)
+    ];
+    const newTree = treeData.filter((node) => !deleteIds.includes(node.id));
+
+    setTreeData(newTree);
+  };
+
+  const handleOpenDialog = () => {
+    setOpen(true);
+  };
+
+  const handleCloseDialog = () => {
+    setOpen(false);
+  };
+
+  const handleSubmit = (newNode) => {
+    const lastId = getLastId(treeData) + 1;
+
+    setTreeData([
+      ...treeData,
+      {
+        ...newNode,
+        id: lastId
+      }
+    ]);
+
+    setOpen(false);
+  };
+
+  const handleTextChange = (id, value) => {
+    const newTree = treeData.map((node) => {
+      if (node.id === id) {
+        return {
+          ...node,
+          text: value
+        };
+      }
+
+      return node;
+    });
+
+    setTreeData(newTree);
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <DndProvider backend={MultiBackend} options={getBackendOptions()}>
+        <div className={styles.app}>
+          <div>
+            <Button onClick={handleOpenDialog} startIcon={<AddIcon />}>
+              Add Node
+            </Button>
+            {open && (
+              <AddDialog
+                tree={treeData}
+                onClose={handleCloseDialog}
+                onSubmit={handleSubmit}
+              />
+            )}
+          </div>
+          <Tree
+            tree={treeData}
+            rootId={0}
+            render={(node, options) => (
+              <CustomNode
+                node={node}
+                {...options}
+                onDelete={handleDelete}
+                onTextChange={handleTextChange}
+              />
+            )}
+            dragPreviewRender={(monitorProps) => (
+              <CustomDragPreview monitorProps={monitorProps} />
+            )}
+            onDrop={handleDrop}
+            classes={{
+              root: styles.treeRoot,
+              draggingSource: styles.draggingSource,
+              dropTarget: styles.dropTarget
+            }}
+          />
+        </div>
+      </DndProvider>
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ src/CustomNode.jsx
@@ -0,0 +1,103 @@
+import React, { useState } from "react";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import IconButton from "@mui/material/IconButton";
+import CheckIcon from "@mui/icons-material/Check";
+import CloseIcon from "@mui/icons-material/Close";
+import EditIcon from "@mui/icons-material/Edit";
+import { ArrowRight, Delete, FileCopy } from "@mui/icons-material";
+import { useDragOver } from "@minoru/react-dnd-treeview";
+import { TypeIcon } from "./TypeIcon";
+import styles from "./CustomNode.module.css";
+
+export const CustomNode = (props) => {
+  const [hover, setHover] = useState(false);
+  const { id, text, onTextChange, droppable, data } = props.node;
+  const [visibleInput, setVisibleInput] = useState(false);
+  const [labelText, setLabelText] = useState(text);
+  const indent = props.depth * 24;
+
+  const handleToggle = (e) => {
+    e.stopPropagation();
+    props.onToggle(props.node.id);
+  };
+
+  const handleShowInput = () => {
+    setVisibleInput(true);
+  };
+
+  const handleCancel = () => {
+    setLabelText(text);
+    setVisibleInput(false);
+  };
+
+  const handleChangeText = (e) => {
+    setLabelText(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    setVisibleInput(false);
+    props.onTextChange(id, labelText);
+  };
+
+  const dragOverProps = useDragOver(id, props.isOpen, props.onToggle);
+
+  return (
+    <div
+      className={`tree-node ${styles.root}`}
+      style={{ paddingInlineStart: indent }}
+      {...dragOverProps}
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    >
+      <div className={`${styles.expandIconWrapper} ${props.isOpen ? styles.isOpen : ""}`}>
+        {props.node.droppable && (
+          <div onClick={handleToggle}>
+            <ArrowRight />
+          </div>
+        )}
+      </div>
+      <div>
+        <TypeIcon droppable={droppable} fileType={data?.fileType} />
+      </div>
+      <div className={styles.labelGridItem}>
+        {visibleInput ? (
+          <div className={styles.inputWrapper}>
+            <TextField
+              className={`${styles.textField}
+              ${styles.nodeInput}`}
+              value={labelText}
+              onChange={handleChangeText}
+            />
+            <IconButton
+              className={styles.editButton}
+              onClick={handleSubmit}
+              disabled={labelText === ""}
+            >
+              <CheckIcon className={styles.editIcon} />
+            </IconButton>
+            <IconButton className={styles.editButton} onClick={handleCancel}>
+              <CloseIcon className={styles.editIcon} />
+            </IconButton>
+          </div>
+        ) : (
+          <div className={styles.labelGridItem}>
+            <Typography variant="body2">{props.node.text}</Typography>
+          </div>
+        )}
+      </div>
+      {hover && !visibleInput && (
+        <>
+          <div className={styles.actionButton}>
+            <IconButton size="small" onClick={() => props.onDelete(id)}>
+              <Delete fontSize="small" />
+            </IconButton>
+            <IconButton size="small" onClick={handleShowInput}>
+              <EditIcon fontSize="small" />
+            </IconButton>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
